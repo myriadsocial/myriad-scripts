5098878d6ba5c1e997fadfe81bc5780fa389ce95
Agustinus Theodorus <agustinustheoo@gmail.com>
2023-10-27 00:11:39 +0700

    feat: update and execute script for airdrops in devnet

---
+++ scripts/rococo-airdrop/airdropList.ts
@@ -0,0 +1,267 @@
+export const airdropAddresses: { address: string, balance: string, networkId: string }[] = [
+  {
+    "address": "5Ckp5p8yDRkLVh8pkSeQHK8xFaojGt4eUjMoHBMKLx7Mwem7",
+    "balance": "3134199999375000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Cr4D7HLFubZUHnWSa2ErcTVqLGXDMcRRECPMJMUJu9euTfU",
+    "balance": "40090304795001589023923",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Di8z8iqwkg1uEz2zqEyJopif8HdVvPT3DCxmyJt8SkBQeFS",
+    "balance": "1023969999999750000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FbfARDLcsMRi5cHHQbFvpJAbMoQRfy736gmA47hPu7dbNPj",
+    "balance": "2835099998875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GEQkADvUbKpYPq4Y7jRSPe6VRm4wyWhaj7KeCW4Gbt1U8y3",
+    "balance": "197955199999625000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5HL5pjEKqe4MGN5c74bqsCH2tywqekn5TSemkRTwjckVaPSx",
+    "balance": "98985099999875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GWPQpH4DeXPjSLu1gvi9J8QuLP2qFP8KLXgsZf9c4qt4hpd",
+    "balance": "98745629999875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5CaKUk4nRYvAXEvsfsGzFwJuav5A4i4EZtVg1Y3UwhtoTe7A",
+    "balance": "18355242999000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5DU6kA5zSdAN7JFb3W32Sk3fwoL69hGUq26R6zMyUefSM8u4",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5G6E2733b6oKboph36bV7a4sX33JkVjL3wG3kJsuuu3pncC4",
+    "balance": "101000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GcAwxjF5SBRaXALDimcyNgwqJHz32HCWeZgN7YNx4Sk5v8y",
+    "balance": "98875699999750000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5ECjEKCPL57YgPeJS1h11wukT8xbN5rCND18e4NzrdZoCBUb",
+    "balance": "93832025399625000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Eq4PzJS6dPmPk87VgmkG3p8oTFZm2ywWY9APW9XdX8bkZKD",
+    "balance": "85878225718625000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5ELCW4TVgncqZpYT3w3VW4rdqtpoE1P3Mvg4WGDhYqiFrT3T",
+    "balance": "60044599999625000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5E7X2fA67kwZANSiq6Gs4aWSzgVYKzzvppp7dKxSktm8j89j",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5E7yipdzJEbg3cgyUWmg6owm6DaWtqvfk7krK1ACTjjUkjHE",
+    "balance": "5505199996375000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5E9bYYbgkhEon1TXmCNQc1Mb9B4YubgTpWiiBU5uTj9oUdbU",
+    "balance": "100500000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5E2jZw5cZMZznuVEgu7Xcbha6LXjcJYLSa7Bj7FTdtMeZYto",
+    "balance": "3726099999500000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5ELL1gDwFxCbrWSRJB7rzV2hAiSB4K5J9Gv7SbxRawKyb9bd",
+    "balance": "6889799997750000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5EYz7X7rtwnQzxcxzWkvnzbN8nNVECkbVJJiiZgKpyAy4rPz",
+    "balance": "1870039997375000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5CJG2QrvSsoRsEZ1iD1ECCKr18Ef4wVR1ed8ytF7wnCumcZy",
+    "balance": "11621651197250000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Hj8xDYdKCf1DQ5fJqkpWGD9LnyXMHsdjhJRM75Z7qypCxYS",
+    "balance": "24873699998750000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5F7XcNy76odoDgbGV5z6khzsXCJjaxvZX5ocirWj2z1R8RvK",
+    "balance": "91223499999538702000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5CHqs1rkr3AcyWvUGeLMf9Nu2Lq5bJimBbDRo2c6Pb8foVNH",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GgumENjZE6XhALsqmfkqZaKZhPNavP9ZrjWP1cmwAPNs76S",
+    "balance": "82909299999375000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GMsdQUbbFTh7xDizheneH23Hmn8KTizUuoTt4o2ZQbT14cH",
+    "balance": "101000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FHjx6Uge54hVu7WHvigr3ALUVRnhaYpBgDqLJ9fpCFzAbLJ",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5DZeuM1XArLLqqHLCm61EoY67EstbAUMRsuT8boGCdtNzpFi",
+    "balance": "89476199999875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5DvfXtRz7q2rB5wYKoGxZVM6vrfys65UNHywptH6PRVHsCnW",
+    "balance": "109500000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5DZaGyuW6pb96E5QwUDMDzopzLTeUD6QRyWzUS3X8UREMuMY",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5HBCW1JWkqjh1ndbEi9WTpVB3rbyMn6R3eCdfFvbaxTzQnur",
+    "balance": "92101709889616106000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FbRzFjF6s7211YNK3sNc8jPjSJCJx9YUNmYQBnre52Q5vrz",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Gmk9h4aeyG7K75EKtp4NXFh6EJU1EadQneLkawtdoqLN3Ad",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FNyEGPbmXf7pDM7DxZriX31peECPnHvf6P8oxrnRmwTSS8j",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5HaqgJ913ZUxtciu4KoTf7WH8wed1zGWEyakrHvFYoMEmBGG",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Gs5scT3bLSDSB1udmhFQSF33vcFHtApwvWzAw5MRP1i1r9M",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5CkKASHffD6PsF7W6AkdLTvHtRrUNCKyaCTgN9SHgJETLj6o",
+    "balance": "89983799999875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5DxsbdyrxKC5zs1dHJWDoZngcPBbNUurLmsPVQx6L5Uo2Fsm",
+    "balance": "999579987522899996485337994",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FNrHLn1Nak7xtRi23UY15N8JWgrEoLDuNnphASSfUtFrQgb",
+    "balance": "25150709999625000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5CPBto2WzA2yfLhPBqNLuL37pAytmozhp2vyr8JESv6Totcx",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Gxe4WHjcJ1F4KSArVp7WJ6LQTUPpMdh1UuAysLP3d16phHM",
+    "balance": "135079224760979569974",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Fxxc7D95wtryrVeWv4ZvAFpkXbrney9qK5KEuFsc3WtmRr7",
+    "balance": "2000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FenkMqKp9kQoen8q3dVcCR1dQBournoYfM2tZd3wBL6Pnmw",
+    "balance": "9972799999827404000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5EZLn1ube7Z3kHmK9two95QuCnvgEWAf6ruUFT7xUGLQAYi5",
+    "balance": "1100000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5Gs8yPWi4ezyVVUSNe2KP5njkVawAEdyJsjUENaPhynCeFXj",
+    "balance": "10000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5HjZoZ4EFXRjYzhWpJstuwPeYz2TFtdVXMdt7GkTzCLug5E9",
+    "balance": "105004847199998934615992",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5GvAU4obdfXyLMzxiWJRsdiRCd7XhVsVvzEh631XK7odsvtc",
+    "balance": "151492484673182833656000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5EC1a3JGW9GAuM3U5enkQHkqiUcf6qGaeiGvPN6mzkbnmTpW",
+    "balance": "41432106189750000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5G1XRmr4W6WRVFxPhrRsjdk3muJcVA3y4qgpEDHLp9Yox8yF",
+    "balance": "50021324272496568510000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5F6tFiDcTMfG3BS5azeZc5eX7FvVY38i8C9xGGPu5Wm6rK1h",
+    "balance": "10002749999875000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FJALcG1dbkH7eNLX4SgQdSxkRYAjD5jM2otY9J2xgUJjEYA",
+    "balance": "24000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5FunTdvuWEzpZaFDWTUwRzP6pUv3WX22rqGhvTmapZfC218Q",
+    "balance": "100000000000000000000",
+    "networkId": "myriad"
+  },
+  {
+    "address": "5D2pwtWGgk6XzbbyiwzERyTEF9ZvSN91ZyYSo77mPRaSsqbN",
+    "balance": "14980599999913702000",
+    "networkId": "myriad"
+  }
+];
--- scripts/rococo-airdrop/index.ts
+++ scripts/rococo-airdrop/index.ts
@@ -1,86 +1,74 @@
-import { ApiPromise, WsProvider } from '@polkadot/api';
+import { MongoClient } from 'mongodb';
 import { Keyring } from '@polkadot/keyring';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { airdropAddresses } from "./airdropList";
+import { writeFile } from 'fs/promises';
 
 async function initiateApiPromise(nodeUrl: string): Promise<ApiPromise> {
+    console.log("Initiating ApiPromise...");
     const wsProvider = new WsProvider(nodeUrl);
     return await ApiPromise.create({ provider: wsProvider });
 }
 
-async function getEligigbleAddresses(api: ApiPromise, accounts: string[]): Promise<{ address: string, balance: string }[]> {
-    const eligibleAddresses: { address: string, balance: string }[] = [];
-
-    for (let address of accounts) {
-        const { nonce, data: balance } = (await api.query.system.account(address)) as any;
-        if (balance.free.gt(api.consts.balances.existentialDeposit)) {
-            eligibleAddresses.push({
-                address,
-                balance: balance.free.toString()
-            });
-        }
-    }
-
-    return eligibleAddresses;
+async function getEligigbleAddresses(): Promise<{ address: string, balance: string }[]> {
+    console.log("Retrieving eligible addresses...")
+    return [...airdropAddresses];
 }
 
 async function sendAirdrop(api: ApiPromise, eligibleAddresses: { address: string, balance: string }[]) {
     const sudoSeed = 'bottom drive obey lake curtain smoke basket hold race lonely fit walk//Alice'; // Alice's seed in dev mode
     const keyring = new Keyring({ type: 'sr25519' });
     const sudoAccount = keyring.addFromUri(sudoSeed);
+    console.log("Create Sudo account...");
+    const failedAirdrops: { address: string, balance: string }[] = [];
 
     for (let dest of eligibleAddresses) {
+        console.log(`Processing Airdrop for ${dest.address}.`);
         const transferExtrinsic = api.tx.balances.transfer(dest.address, dest.balance);
 
         try {
             const hash = await transferExtrinsic.signAndSend(sudoAccount, { nonce: -1 });
             console.log(`Airdropped to ${dest.address}. Transaction hash: ${hash.toHex()}`);
+
+            // TODO: FIX ME cannot connect to DB and blockchain at the same time.
+            // Either that or execute from inside server
+            // await storeAirdroppedAddresses(dest);
         } catch (error) {
             console.error(`Error airdropping to ${dest.address}:`, error);
+
+            failedAirdrops.push(dest);
         }
     }
+
+    return failedAirdrops;
 }
 
-// async function initiateMongoConnection() {
-//     const mongoUrl = 'mongodb://yourMongoDbUrl:port';
-//     const client = new MongoClient(mongoUrl, { useUnifiedTopology: true });
+const DB_URL = 'mongodb://localhost:27017';
 
-//     await client.connect();
-//     return client.db('yourDbName');
-// }
+async function initiateMongoConnection() {
+    const client = new MongoClient(DB_URL);
+    await client.connect();
+    return client.db('myriad');
+}
 
-// async function getAccountsFromMongoDB(): Promise<string[]> {
-//     const db = await initiateMongoConnection();
-//     const collection = db.collection('yourCollectionName');
-//     const accounts = await collection.find({}).toArray();
-//     return accounts.map(acc => acc.address);
-// }
-
-// async function storeAirdroppedAddresses(addresses: { address: string, balance: string }[]) {
-//     const db = await initiateMongoConnection();
-//     const collection = db.collection('airdroppedAddresses');
-//     await collection.insertMany(addresses);
-// }
+async function storeAirdroppedAddresses(addresses: { address: string, balance: string }) {
+    const db = await initiateMongoConnection();
+    const collection = db.collection('airdroppedAddressesDevnet');
+    await collection.insertOne(addresses);
+}
 
 async function airdropToAccounts() {
-    const nodeUrl = 'ws://localhost:9944'; // Octopus Appchain
-    const api = await initiateApiPromise(nodeUrl);
-
-    // Sample list of accounts you'd like to check.
-    // In reality, you might need a more complex approach to fetch all accounts.
-    // TODO: Get from MongoDB Offchain DB the list of addresses registered for available airdrop
-    const accounts = ['5HmXRcRc656J9QDDPnvC186E1rav4ydPS3LJ9vXy5QZTxtLK']; 
-
-    const eligibleAddresses = await getEligigbleAddresses(api, accounts);
-    await api.disconnect();
+    const eligibleAddresses = await getEligigbleAddresses();
 
     console.log("Eligible addresses for airdrop:", eligibleAddresses);
 
-    const nodeUrl2 = 'ws://localhost:9944'; // Rococo Testnet
-    const api2 = await initiateApiPromise(nodeUrl2);
+    const nodeUrl = 'wss://ws-rpc.devnet.myriad.social/websocket/'; // Rococo Devnet
+    const api = await initiateApiPromise(nodeUrl);
 
-    await sendAirdrop(api2, eligibleAddresses);
-    await api2.disconnect();
+    const failedAirdrops = await sendAirdrop(api, eligibleAddresses);
+    await api.disconnect();
 
-    // TODO: Create a collection of addresses of assets airdropped from this migration script
+    await writeFile('failed_airdrops.json', JSON.stringify(failedAirdrops, null, 2), 'utf8');
 }
 
 airdropToAccounts()
